d9cbec7750c900feb0cb6caf9b6be3a1a5f58c4c
Tasneem Hannah Manga <tasneemhannahmanga@gmail.com>
2024-12-03 10:31:33 +0200

    undoing react.strictmode

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
