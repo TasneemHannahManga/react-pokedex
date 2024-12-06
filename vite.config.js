8f66eb9274ab4f57af479ef631e3002224fd4178
Tasneem Hannah Manga <tasneemhannahmanga@gmail.com>
2024-12-06 20:55:50 +0200

    404.html and dependancies

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/react-pokedex/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
 });
