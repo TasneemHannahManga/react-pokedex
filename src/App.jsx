c340dfec736b3d07c098b865998ff2f93039c37e
Tasneem Hannah Manga <tasneemhannahmanga@gmail.com>
2024-12-09 15:12:56 +0200

    feat: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import Header from "./App-components/Header";
-import RandomPokémonGenerator from "./App-components/Pages/RandomPokémonGenerator";
-import Home from "./App-components/Pages/Home";
-import PokePage from "./App-components/Pages/PokePage";
 import Footer from "./App-components/Footer"; 
-import { BrowserRouter as Routes, Route, HashRouter } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <HashRouter basename="/react-pokedex/">
-        <Header /> 
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/random" element={<RandomPokémonGenerator />} />
-          <Route exact path="/pokemon/:id" element={<PokePage />} />
-        </Routes>
-        <Footer />
-      </HashRouter>
+      <Header />
+      <nav>
+        <Link to="/react-pokedex/">Home</Link>
+        {" | "}
+        <Link to="/react-pokedex/random">RandomPokémonGenerator</Link>
+        <Link to="/react-pokedex/:id">PokePage</Link>
+      </nav>
+
+      <Outlet />
+      <Footer />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import RandomPokémonGenerator from "./App-components/Pages/RandomPokémonGenerator";
+import Home from "./App-components/Pages/Home";
+import PokePage from "./App-components/Pages/PokePage";
 import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/react-pokedex/",
+    element: <App />,
+    children: [
+      {
+        path: "/react-pokedex/",
+        element: <Home />,
+      },
+      {
+        path: "/react-pokedex/random",
+        element: <RandomPokémonGenerator />,
+      },
+      {
+        path: "/react-pokedex/:id",
+        element: <PokePage />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
-    <App />
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
